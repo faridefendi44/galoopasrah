af89e7c444cc0fcf64ee2602b2e8af714ccc999d
Moch Danial Rasyid Akbar S <danialrasyid1@student.ub.ac.id>
2023-03-20 14:21:29 +0700

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 
 class Payment extends Model
 {
@@ -18,7 +20,7 @@ class Payment extends Model
 
 	public const EXPIRY_DURATION = 1;
 	public const EXPIRY_UNIT = 'days';
-	  
+
 
 	public const CHALLENGE = 'challenge';
 	public const SUCCESS = 'success';
@@ -35,10 +37,10 @@ class Payment extends Model
 	{
 		$integer = intval($integer);
 		$result = '';
-		
+
 		// Create a lookup array that contains all of the Roman numerals.
 		$lookup = ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
- 
+
 		foreach ($lookup as $roman => $value) {
 			$matches = intval($integer/$value);
 			$result .= str_repeat($roman, $matches);
@@ -52,17 +54,17 @@ class Payment extends Model
 	{
 		$dateCode = self::PAYMENTCODE . '/' . date('Ymd') . '/' . self::integerToRoman(date('m')). '/' . self::integerToRoman(date('d')). '/';
 
-		$lastOrder = self::select([\DB::raw('MAX(payments.number) AS last_code')])
+		$lastOrder = self::select([DB::raw('MAX(payments.number) AS last_code')])
 			->where('number', 'like', $dateCode . '%')
 			->first();
 
 		$lastOrderCode = !empty($lastOrder) ? $lastOrder['last_code'] : null;
-		
+
 		$orderCode = $dateCode . '00001';
 		if ($lastOrderCode) {
 			$lastOrderNumber = str_replace($dateCode, '', $lastOrderCode);
 			$nextOrderNumber = sprintf('%05d', (int)$lastOrderNumber + 1);
-			
+
 			$orderCode = $dateCode . $nextOrderNumber;
 		}
 
